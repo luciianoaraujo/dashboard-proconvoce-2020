6156bd6b6bda57e0a34ec97a11e9c25617d96198
Luciano José de Araújo <luciano.9912@gmail.com>
2020-07-28 10:12:34 -0300

    Menu Lateral de pesquisa, atendimento e navegação

--- src/app/theme/layout/admin/navigation/navigation.ts
+++ src/app/theme/layout/admin/navigation/navigation.ts
@@ -28,6 +28,343 @@ export interface Navigation extends NavigationItem {
 const NavigationItems = [
   {
     id: 'navigation',
+    title: 'Navegação',
+    type: 'group',
+    icon: 'feather icon-monitor',
+    children: [
+      {
+        id: 'dashboard',
+        title: 'Inicio',
+        type: 'item',
+        url: '/dashboard/analytics',
+        icon: 'feather icon-home',
+        breadcrumbs: false
+      },
+    ]
+  },
+  {
+    id: 'pesquisas',
+    title: 'Pesquias',
+    type: 'group',
+    icon: 'feather icon-monitor',
+    children: [
+      {
+        id: 'combustivel',
+        title: 'Combustiveis',
+        type: 'collapse',
+        icon: 'feather icon-battery-charging',
+        children: [
+          {
+            id: 'geral',
+            title: 'Geral',
+            type: 'item',
+            url: '/combustivel/geral',
+            target: true
+          },
+          {
+            id: 'gasolina',
+            title: 'Gasolina',
+            type: 'item',
+            url: '/combustivel/gasolina',
+            target: true
+          },
+          {
+            id: 'etanol',
+            title: 'Etanol',
+            type: 'item',
+            url: '/combustivel/etanol',
+            target: true
+          }
+          ,
+          {
+            id: 'diesel',
+            title: 'Diesel',
+            type: 'item',
+            url: '/combustivel/diesel',
+            target: true
+          }
+        ]
+      },
+      {
+        id: 'escolar',
+        title: 'Escolar',
+        type: 'collapse',
+        icon: 'feather icon-book',
+        children: [
+          {
+            id: 'geral',
+            title: 'Geral',
+            type: 'item',
+            url: '/escolar/geral',
+            target: true
+          },
+          {
+            id: 'bercario',
+            title: 'Berçário',
+            type: 'item',
+            url: '/escolar/bercario',
+            target: true
+          },
+          {
+            id: 'infantil',
+            title: 'Ensino Infantil',
+            type: 'item',
+            url: '/escolar/infantil',
+            target: true
+          },
+          {
+            id: 'fundamental1',
+            title: 'Ensino Fundamental I',
+            type: 'item',
+            url: '/escolar/fundamental1',
+            target: true
+          },
+          {
+            id: 'fundamental2',
+            title: 'Ensino Fundamental II',
+            type: 'item',
+            url: '/escolar/fundamental2',
+            target: true
+          },
+          {
+            id: 'medio',
+            title: 'Ensino Médio',
+            type: 'item',
+            url: '/escolar/medio',
+            target: true
+          },
+          {
+            id: 'prevestibular',
+            title: 'Pré-Vestibular',
+            type: 'item',
+            url: '/escolar/prevestibular',
+            target: true
+          }
+        ]
+      },
+      {
+        id: 'bebidas',
+        title: 'Bebidas',
+        type: 'collapse',
+        icon: 'feather icon-filter',
+        children: [
+          {
+            id: 'geral',
+            title: 'Geral',
+            type: 'item',
+            url: '/escolar/geral',
+            target: true
+          },
+          {
+            id: 'cerveja',
+            title: 'Cerveja',
+            type: 'item',
+            url: '/escolar/bercario',
+            target: true
+          },
+          {
+            id: 'destilados',
+            title: 'Destilados',
+            type: 'item',
+            url: '/bebidas/destilados',
+            target: true
+          },
+          {
+            id: 'refrireranteeenergetico',
+            title: 'Refrigerante e Energético',
+            type: 'item',
+            url: '/bebidas/refrireranteeenergetico',
+            target: true
+          },
+          {
+            id: 'sucos',
+            title: 'Sucos',
+            type: 'item',
+            url: '/bebidas/sucos',
+            target: true
+          }
+        ]
+      },
+      {
+        id: 'churrasco',
+        title: 'Churrasco',
+        type: 'collapse',
+        icon: 'feather icon-music',
+        children: [
+          {
+            id: 'geral',
+            title: 'Geral',
+            type: 'item',
+            url: '/churrasco/geral',
+            target: true
+          },
+          {
+            id: 'carnes',
+            title: 'Carnes',
+            type: 'item',
+            url: '/churrasco/carnes',
+            target: true
+          },
+          {
+            id: 'cortesespeciais',
+            title: 'Cortes Especiais',
+            type: 'item',
+            url: '/churrasco/cortesespeciais',
+            target: true
+          },
+          {
+            id: 'carvao',
+            title: 'Carvão',
+            type: 'item',
+            url: '/churrasco/carvao',
+            target: true
+          },
+          {
+            id: 'acompanhamentos',
+            title: 'Acompanhamentos',
+            type: 'item',
+            url: '/churrasco/acompanhamentos',
+            target: true
+          }
+        ]
+      },
+      {
+        id: 'autoescola',
+        title: 'Auto Escola',
+        type: 'collapse',
+        icon: 'feather icon-search',
+        children: [
+          {
+            id: 'geral',
+            title: 'Geral',
+            type: 'item',
+            url: '/autoescola/geral',
+            target: true
+          },
+          {
+            id: '1habilitacaocarro',
+            title: '1ª Habilitacao Carro (B)',
+            type: 'item',
+            url: '/autoescola/1habilitacaocarro',
+            target: true
+          },
+          {
+            id: '1habilitacaomoto',
+            title: '1ª Habilitacao Moto (A)',
+            type: 'item',
+            url: '/autoescola/1habilitacaomoto',
+            target: true
+          },
+          {
+            id: 'categoriab',
+            title: 'Adição de Categoria B',
+            type: 'item',
+            url: '/autoescola/categoriab',
+            target: true
+          },
+          {
+            id: 'categoriaa',
+            title: 'Adição de Categoria A',
+            type: 'item',
+            url: '/autoescola/categoriaa',
+            target: true
+          },
+          {
+            id: 'mudanca',
+            title: 'Mudança de Categoria',
+            type: 'item',
+            url: '/autoescola/mudanca',
+            target: true
+          },
+          {
+            id: 'outros',
+            title: 'Outros Serviços',
+            type: 'item',
+            url: '/autoescola/outros',
+            target: true
+          }
+        ]
+      },
+      {
+        id: 'cestabasica',
+        title: 'Cesta Básica',
+        type: 'item',
+        url: '/cestabasica/geral',
+        icon: 'feather icon-shopping-cart',
+        breadcrumbs: false
+      },
+      {
+        id: 'gas',
+        title: 'Gás de Cozinha',
+        type: 'item',
+        url: '/gas/geral',
+        icon: 'feather icon-zap',
+        breadcrumbs: false
+      },
+    ]
+  },
+  {
+    id: 'atendimento',
+    title: 'Atendimento',
+    type: 'group',
+    icon: 'feather icon-monitor',
+    children: [
+      {
+        id: 'duvidas',
+        title: 'Dúvidas',
+        type: 'item',
+        url: '/duvidas/',
+        icon: 'feather icon-home',
+        breadcrumbs: false
+      },
+      {
+        id: 'reclamacao',
+        title: 'Reclamações',
+        type: 'item',
+        url: '/reclamacao/',
+        icon: 'feather icon-home',
+        breadcrumbs: false
+      },
+      {
+        id: 'denuncia',
+        title: 'Denúncias',
+        type: 'collapse',
+        icon: 'feather icon-layout',
+        children: [
+          {
+            id: 'geral',
+            title: 'Geral',
+            type: 'item',
+            url: '/denuncia/geral',
+            target: true
+          },
+          {
+            id: 'comum',
+            title: 'Comum',
+            type: 'item',
+            url: '/denuncia/comum',
+            target: true
+          },
+          {
+            id: 'bancos',
+            title: 'Bancos',
+            type: 'item',
+            url: '/autoescola/bancos',
+            target: true
+          },
+          {
+            id: 'transporte',
+            title: 'Transporte Público',
+            type: 'item',
+            url: '/autoescola/transporte',
+            target: true
+          }
+        ]
+      }
+    ]
+  },
+  {
+    id: 'navigation',
     title: 'Navigation',
     type: 'group',
     icon: 'feather icon-monitor',
